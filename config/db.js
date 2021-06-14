8118d32ad2e75d1094b2860e765ce0edf6d2526f
ducthien19052000 <ducthien19052000@gmail.com>
2021-06-14 16:41:24 +0700

    data

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async()=>{
 
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
+        await mongoose.connect('mongodb+srv://user-123:user-123@cluster0.tuxc2.mongodb.net/shop?retryWrites=true&w=majority',{
             useNewUrlParser:true,
             useUnifiedTopology:true
         })
